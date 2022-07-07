e4c1fad30d2778263761e0b43fd17e6377c6b34b
Cause Chung <cuzfrog@gmail.com>
2022-07-07 08:26:27 +0800

    Add more utils func and refine SliceFrom

--- utils/funcs.go
+++ utils/funcs.go
@@ -1,5 +1,37 @@
 package utils
 
+import (
+	"golang.org/x/exp/constraints"
+	"math/rand"
+)
+
 func Nil[T any]() (v T) {
 	return v
+}
+
+func Min[T constraints.Ordered](l, r T) (m T) {
+	if l < r {
+		m = l
+	} else {
+		m = r
+	}
+	return
+}
+
+func Max[T constraints.Ordered](l, r T) (m T) {
+	if l > r {
+		m = l
+	} else {
+		m = r
+	}
+	return
+}
+
+func Suffle[T any](a []T) {
+	n := len(a)
+	var j int
+	for i := range a {
+		j = rand.Intn(n)
+		a[i], a[j] = a[j], a[i]
+	}
 }
--- utils/iteration.go
+++ utils/iteration.go
@@ -1,11 +1,21 @@
 package utils
 
-import "github.com/cuzfrog/tgods/core"
+import (
+	"github.com/cuzfrog/tgods/core"
+)
 
-func Slice[T any](it core.Iterator[T], size int) []T {
+// SliceFrom creates a new slice from the Iterator,
+// len(slice) = min(size, elemCntFromIterator)
+// cap(slice) = size
+func SliceFrom[T any](it core.Iterator[T], size int) []T {
 	arr := make([]T, size)
+	var i int
 	for it.Next() {
-		arr[it.Index()] = it.Value()
+		i = it.Index()
+		if i >= size {
+			break
+		}
+		arr[i] = it.Value()
 	}
-	return arr
+	return arr[:Min(i+1, size)]
 }
