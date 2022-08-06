516f7c5740ce39470542a6165128592573a741f6
Cause Chung <cuzfrog@139.com>
2022-08-06 17:59:39 +0800

    Add methods SliceProject

--- utils/arrays_test.go
+++ utils/arrays_test.go
@@ -11,3 +11,9 @@ func TestSlice(t *testing.T) {
 	arr := SliceFrom[int](c)
 	assert.Equal(t, []int{1, 4, 3, 2}, arr)
 }
+
+func TestSliceProjection(t *testing.T) {
+	c := mocks.NewMockCollectionOf[int](1, 4, 3, 2)
+	arr := SliceProject[int, int](c, func(v int) int { return v + 1 })
+	assert.Equal(t, []int{2, 5, 4, 3}, arr)
+}
