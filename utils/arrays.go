418697f4a540863256d8537f9f93728076ee7be2
Cause Chung <cuzfrog@139.com>
2022-08-06 17:49:51 +0800

    Add method SliceFromIterator

--- utils/arrays.go
+++ utils/arrays.go
@@ -4,11 +4,16 @@ import (
 	"github.com/cuzfrog/tgods/types"
 )
 
-// SliceFrom creates a new slice from the Iterator,
-// len(slice) = min(size, elemCntFromIterator)
-// cap(slice) = size
+// SliceFrom creates a new slice from the Collection,
+//   len(slice) = min(size, elemCntFromIterator)
+//   cap(slice) = size
 func SliceFrom[T any](col types.Collection[T]) []T {
 	it, size := col.Iterator(), col.Size()
+	return SliceFromIterator(it, size)
+}
+
+// SliceFromIterator creates a new slice from the Iterator,
+func SliceFromIterator[T any](it types.Iterator[T], size int) []T {
 	arr := make([]T, size)
 	for it.Next() {
 		i := it.Index()
