6fff68840ecaa1c697fe57db16021cec6e9f7bf4
Cause Chung <cuzfrog@139.com>
2022-10-25 23:23:00 +0800

    impl utils.AddAll

---
+++ utils/collections.go
@@ -0,0 +1,16 @@
+package utils
+
+import "github.com/cuzfrog/tgods/types"
+
+// AddAll adds all elems from src collection into target collection, return number of elems actually added
+func AddAll[T any](src types.Collection[T], target types.Collection[T]) int {
+	cnt := 0
+	itor := src.Iterator()
+	for itor.Next() {
+		added := target.Add(itor.Value())
+		if added {
+			cnt++
+		}
+	}
+	return cnt
+}
