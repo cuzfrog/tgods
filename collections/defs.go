ff1330a15cc976a68d3574ce57ca69d51a7c514c
Cause Chung <cuzfrog@139.com>
2022-12-09 10:42:53 +0800

    add AutoSizingFlag const

--- collections/defs.go
+++ collections/defs.go
@@ -19,3 +19,12 @@ const (
 	PutOrder      AccessOrder = 1 // newly put element will be at the tail (the elem at head will be next for eviction).
 	GetOrder      AccessOrder = 2 // newly get element will be at the tail (the elem at head will be next for eviction).
 )
+
+// AutoSizingFlag default is AutoExpand + AutoShrink
+type AutoSizingFlag byte
+
+const (
+	NoAutoSizing AutoSizingFlag = 0 // no auto expansion or auto shrinking. Panic if cap is not enough.
+	AutoExpand   AutoSizingFlag = 1 // auto expand internal memory data structure to increase cap.
+	AutoShrink   AutoSizingFlag = 2 // auto shrink internal memory data structure to decrease cap.
+)
